041e3216d71403384ca6ac544825b11d87a8af3c
sabrinagiometto <sabrina.giometto@gmail.com>
2023-09-02 16:13:16 +0200

    Path changed to a standard one. Just need to have a data folder including datasets within the R project, wherever is located.

--- R/wtd_bbmle_try.R
+++ R/wtd_bbmle_try.R
@@ -3,7 +3,7 @@ library(bbmle)
 library(haven)
 
 
-wtddat <- read_dta("d:/stovring/PharmacoEpid/R_wtdttt/wtddat_covar.dta")
+wtddat <- read_dta("data/wtddat_covar.dta")
 
 wtddat <- mutate(wtddat,
        wtdtimes = 1 - last_rxtime,
@@ -11,13 +11,13 @@ wtddat <- mutate(wtddat,
 
 delta <- 1
 
-dwtdttt <- function(x, logitp, mu, lnsigma, log = FALSE) 
+dwtdttt <- function(x, logitp, mu, lnsigma, log = FALSE)
 {
   prob <- exp(logitp) / (1 + exp(logitp))
   sigma <- exp(lnsigma)
   mean <- exp(mu + exp(2 * lnsigma)/2)
   if (log == FALSE) {
-    prob * pnorm(log(x), mu, sigma, lower.tail = FALSE) / mean 
+    prob * pnorm(log(x), mu, sigma, lower.tail = FALSE) / mean
       + (1 - prob) / delta
   }
   else {
@@ -27,14 +27,14 @@ dwtdttt <- function(x, logitp, mu, lnsigma, log = FALSE)
 }
 
 ## Testing the density function - does it make sense?
-wtddat$wtddens <- dwtdttt(wtddat$wtdtimes, 
+wtddat$wtddens <- dwtdttt(wtddat$wtdtimes,
                           logitp = 1, mu = log(.15), lnsigma = -0.4, log = FALSE)
 ggplot(data = wtddat) +
   geom_point(mapping = aes(x=wtdtimes, y=wtddens))
 
 
 ## Covariates can now be included (SIC!!!)
-fit1 <- mle2(wtdtimes ~ dwtdttt(logitp, mu, lnsigma), 
+fit1 <- mle2(wtdtimes ~ dwtdttt(logitp, mu, lnsigma),
              parameters = list(logitp ~ packcat, mu ~ packcat, lnsigma ~ 1),
              start = list(logitp = 0, mu = log(delta/5), lnsigma = 0),
              data = wtddat)

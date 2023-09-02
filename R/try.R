802dc082c578d679fec1f894d217f0d227fbe475
sabrinagiometto <sabrina.giometto@gmail.com>
2023-09-02 14:33:21 +0200

    change try

--- R/try.R
+++ R/try.R
@@ -1,9 +1,13 @@
 library(tidyverse)
 library(bbmle)
 library(haven)
+library(tidyverse)
 
 
 wtddat <- read_dta("d:/stovring/PharmacoEpid/R_wtdttt/wtddat_covar.dta")
+
+wtddat %>%
+  filter(packsize>=200)
 
 wtddat <- mutate(wtddat,
                  wtdtimes = 1 - last_rxtime,

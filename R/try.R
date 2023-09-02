a6aa09d4f221e95faf07dd0804c38f57f7862730
sabrinagiometto <137386371+sabrinagiometto@users.noreply.github.com>
2023-09-02 14:35:30 +0200

    Update try.R

--- R/try.R
+++ R/try.R
@@ -9,6 +9,11 @@ wtddat <- read_dta("d:/stovring/PharmacoEpid/R_wtdttt/wtddat_covar.dta")
 wtddat %>%
   filter(packsize>=200)
 
+# change to 150
+
+wtddat %>%
+  filter(packsize>=150)
+
 wtddat <- mutate(wtddat,
                  wtdtimes = 1 - last_rxtime,
                  packcat = factor(packsize))

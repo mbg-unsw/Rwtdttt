09f581f5cb6c0054bf1f72ccee1e831fa990219f
sabrinagiometto <sabrina.giometto@gmail.com>
2023-12-10 22:27:21 +0100

    Function calls

--- sandpit/try.R
+++ sandpit/try.R
@@ -1,19 +1,21 @@
-library(tidyverse)
-library(bbmle)
-library(haven)
-library(tidyverse)
+rm(list = ls())
 
+# load data
+df <- read_dta("data/wtddat_dates.dta")
+df <- as.data.table(df)
 
-wtddat <- read_dta("d:/stovring/PharmacoEpid/R_wtdttt/wtddat_covar.dta")
+# create time variable
+create_time(data = df, event.date.colname = rx1time, start = as.Date('2014-01-01'))
 
-wtddat %>%
-  filter(packsize>=200)
+# fit waiting time distribution
+fit1 <- wtdttt(obstime ~ dlnorm(logitp, mu, lnsigma),
+               event.date.colname = rx1time,
+               event.time.colname = obstime,
+               start = as.Date('2014-01-01'), end = as.Date('2014-12-31'),
+               init = list(logitp = 1.65, mu = 4.23, lnsigma = -1.26),
+               data = df)
 
-# change to 150
+summary(fit1)
 
-wtddat %>%
-  filter(packsize>=150)
-
-wtddat <- mutate(wtddat,
-                 wtdtimes = 1 - last_rxtime,
-                 packcat = factor(packsize))
+predict(fit1, family = "lnorm")
+predict(fit1, family = "lnorm", type = "prob", distrx=c(2,100))

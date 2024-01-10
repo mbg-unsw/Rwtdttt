594cf6c49c0c7f459d94a778035b81c214cd5370
sabrinagiometto <sabrina.giometto@gmail.com>
2024-01-10 17:13:16 +0100

    Part of random index date started

--- sandpit/try.R
+++ sandpit/try.R
@@ -1,15 +1,15 @@
 rm(list = ls())
 
 # load data
-df <- read_dta("data/wtddat_dates.dta")
+df <- haven::read_dta("data/wtddat_dates.dta")
 df <- as.data.table(df)
 
 # create time variable
 test <- create_time(data = df, event.date.colname = rx1time, start = as.Date('2014-01-01'))
 
 # fit waiting time distribution
-fit1 <- wtdttt(obstime ~ dlnorm(logitp, mu, lnsigma),
-               data = df,
+fit1 <- wtdttt(data = df,
+               obstime ~ dlnorm(logitp, mu, lnsigma),
                event.date.colname = rx1time,
                event.time.colname = obstime,
                start = as.Date('2014-01-01'), end = as.Date('2014-12-31'),
@@ -20,3 +20,23 @@ summary(fit1)
 
 predict(fit1, family = "lnorm")
 predict(fit1, family = "lnorm", type = "prob", distrx=c(2,100))
+
+########################### RANDOM INEDX DATE
+
+# by hand
+
+# create sampling window and define a random index date for each individual
+set.seed(84)
+
+df <- df %>%
+       mutate(r_index_date = sample(as.Date(as.Date("2014-01-01"):as.Date("2014-12-31")), nrow(df), replace=T),
+              obstime = as.numeric(rx1time - r_index_date))
+
+# remove dispensations not within the observation window
+df <- df %>%
+       filter(obstime>=0)
+
+################
+
+# calling the created function
+test <- create_time_random(data = df, event.date.colname = rx1time, index.date.colname = r_index_date)

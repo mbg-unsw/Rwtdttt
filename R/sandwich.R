ceca3a83f00c4d6b397b54316868948d7a8300a5
Malcolm Gillies <Malcolm.Gillies@unsw.edu.au>
2024-03-28 21:45:36 +1100

    Further work on sandwich estimation (not finished) Updated sandwich.R sand_vcov() so it loads and runs UNTESTED Added code to sand_vcov() to handle calls with zero, part or all parameters specified as linear models

--- R/sandwich.R
+++ R/sandwich.R
@@ -11,9 +11,38 @@
 #' @examples
 sand_vcov <- function(fit) {
   # parse model formula components
-  parm_form <- unlist(strsplit(unlist(strsplit(fit@formula, ":", fixed=T))[2], ",", fixed=T))
+  parm_form <- unlist(strsplit(gsub(unlist(strsplit(fit@formula, ":", fixed=T))[2], " ", ""), ",", fixed=T))
+
+#####
+# based on code from bbmle::calc_mle2_function
+# FIXME: redo without special case code for each density type
+# FIXME: redo to handle non-standard parameter naming
+
+  parnames <- names(fit@call$start)
+
+  if (!is.na(parm_form) && length(parm_form)>0) {
+    ## linear model specified for some parameters
+    vars <- sapply(strsplit(parm_form, "~", fixed=T),"[",1)
+    models <-  sapply(strsplit(parm_form, "~", fixed=T),"[",2)
+  } else {parm_form <- c(); vars <- c(); models <- c()}
+
   # FIXME for any missing components, substitute "~1"
-    score_mat <- t(sapply(1:nrow(fit@data),
+
+    for (i in seq(along=parnames)) {
+      if (!(parnames[i] %in% vars)) {
+        vars <- c(vars, parnames[i])
+        models <- c(models, "~1")
+        parm_form <- c(parm_form, paste0(parnames[i], "~1"))
+      }
+    }
+
+    vpos <- list()
+    for (i in seq(along=parm_form)) {
+      vname <- vars[i]      ## name of variable
+      vpos[[vname]] <- which(parnames==vname)
+    }
+
+    score_mat <- t(sapply(1:length(fit@data[[1]]),
 
                         function(i) {
 
@@ -22,15 +51,14 @@ sand_vcov <- function(fit) {
                             if(fit@dist=="lnorm") {
 
                               # FIXME this is very ugly
-                              # FIXME check order of columns in fit@coef
-                              # FIXME check order of parm_form components
                               # XXXX can we use fit@minuslogl somehow instead?
 
-                              mm1 <- model.matrix(formula(parm_form[1]), data=fit@data)[i,]
-                              mm2 <- model.matrix(formula(parm_form[2]), data=fit@data)[i,]
-                              mm3 <- model.matrix(formula(parm_form[3]), data=fit@data)[i,]
 
-                              dlnorm(as.numeric(fit@data[i, fit@depvar]),
+                              mm1 <- model.matrix(formula(models[vpos[["logitp"]]]), data=as.data.frame(fit@data))[i,,drop=F]
+                              mm2 <- model.matrix(formula(models[vpos[["mu"]]]), data=as.data.frame(fit@data), drop=F)[i,,drop=F]
+                              mm3 <- model.matrix(formula(models[vpos[["lnsigma"]]]), data=as.data.frame(fit@data), drop=F)[i,,drop=F]
+
+                              dlnorm(as.numeric(getElement(fit@data, fit@depvar)[i]),
 
                                       logitp=t(x[1:ncol(mm1)]) %*% mm1,
 
@@ -44,11 +72,11 @@ sand_vcov <- function(fit) {
 
                           } else if(fit@dist=="weib") {
 
-                            mm1 <- model.matrix(formula(parm_form[1]), data=fit@data)[i,]
-                            mm2 <- model.matrix(formula(parm_form[2]), data=fit@data)[i,]
-                            mm3 <- model.matrix(formula(parm_form[3]), data=fit@data)[i,]
+                            mm1 <- model.matrix(formula(models[vpos[["logitp"]]]), data=as.data.frame(fit@data))[i,,drop=F]
+                            mm2 <- model.matrix(formula(models[vpos[["lnalpha"]]]), data=as.data.frame(fit@data))[i,,drop=F]
+                            mm3 <- model.matrix(formula(models[vpos[["lnbeta"]]]), data=as.data.frame(fit@data))[i,,drop=F]
 
-                            dweib(as.numeric(fit@data[i, fit@depvar]),
+                            dweib(as.numeric(getElement(fit@data, fit@depvar)[i]),
 
                                    logitp=t(x[1:ncol(mm1)]) %*% mm1,
 
@@ -62,10 +90,10 @@ sand_vcov <- function(fit) {
 
                          } else if(fit@dist=="exp") {
 
-                          mm1 <- model.matrix(formula(parm_form[1]), data=fit@data)[i,]
-                          mm2 <- model.matrix(formula(parm_form[2]), data=fit@data)[i,]
+                          mm1 <- model.matrix(formula(models[vpos[["logitp"]]]), data=as.data.frame(fit@data))[i,,drop=F]
+                          mm2 <- model.matrix(formula(models[vpos[["lnbeta"]]]), data=as.data.frame(fit@data))[i,,drop=F]
 
-                          dexp(as.numeric(fit@data[i, fit@depvar]),
+                          dexp(as.numeric(getElement(fit@data, fit@depvar)[i]),
 
                                  logitp=t(x[1:ncol(mm1)]) %*% mm1,
 
@@ -92,13 +120,13 @@ sand_vcov <- function(fit) {
 
   applyTapplySum <- function(X,index) apply(X, 2, function(col) tapply(col, index, sum))
 
-  psi <- applyTapplySum(as.matrix(score_mat), fit@data[, fit@idvar]) # sum by cluster
+  psi <- applyTapplySum(as.matrix(score_mat), getElement(fit@data, fit@idvar)) # sum by cluster
 
   meat <- crossprod(as.matrix(psi))
 
   rownames(meat) <- colnames(meat) <- colnames(psi)
 
-  nid <- NROW(unique(fit@data[, fit@idvar]))
+  nid <- NROW(unique(getElement(fit@data, fit@idvar)))
 
   (nid/(nid-1)) * (bread %*% meat %*% bread)
 }

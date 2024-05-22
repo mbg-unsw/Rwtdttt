b8adffe97decfe3ac0312f72524ba152e91ab190
sabrinagiometto <sabrina.giometto@gmail.com>
2024-05-22 16:01:02 +0200

    covariates labels added when listing duration estimates
    changes extended to weib and exp cases
    
    typo addressed in weib and exp cases
    
    name of newdata parameter changed into prediction.data

--- R/pred_dur_prob.R
+++ R/pred_dur_prob.R
@@ -11,7 +11,7 @@ NULL
 #' the duration of observed prescription redemptions.
 #'
 #' @param object a fitted object of class inheriting from "wtd"
-#' @param newdata An optional data frame in which to look for variables with
+#' @param prediction.data An optional data frame in which to look for variables with
 #' which to predict. If omitted, the fitted values are used.
 #' @param type "dur" or "prob". Default "dur".
 #' @param distrx For type="prob", a vector of dispensing gaps.
@@ -25,41 +25,41 @@ NULL
 #' @export
 #' @importFrom stats pnorm pweibull pexp
 setMethod("predict", "wtd",
-          function(object, newdata=NULL, type="dur", iadmean=F, distrx=NULL, quantile=0.8,
+          function(object, prediction.data=NULL, type="dur", iadmean=F, distrx=NULL, quantile=0.8,
                    se.fit=FALSE, na.action=na.pass, ...) {
+
+            parm_form <- unlist(strsplit(gsub(" ", "", unlist(strsplit(object@formula, ":", fixed=T))[2]), ",", fixed=T))
+
+            # !! parnames e vars sono uguali (ne tengo solo uno)
+
+            parnames <- grep("delta", names(object@call$start), value=T, fixed=T, invert=T)
+
+            if (typeof(parm_form)=="character" && length(parm_form)>0) {
+              ## linear model specified for some parameters
+              vars <- sapply(strsplit(parm_form, "~", fixed=T),"[",1)
+              models <-  paste0("~", sapply(strsplit(parm_form, "~", fixed=T),"[",2))
+            } else {parm_form <- c(); vars <- c(); models <- c()}
+
+            for (i in seq(along=parnames)) {
+              if (!(parnames[i] %in% vars)) {
+                vars <- c(vars, parnames[i])
+                models <- c(models, "~1")
+                parm_form <- c(parm_form, paste0(parnames[i], "~1"))
+              }
+            }
+
+            vpos <- list()
+            for (i in seq(along=parm_form)) {
+              vname <- vars[i]      ## name of variable
+              vpos[[vname]] <- which(parnames==vname)
+            }
+
+            # browser()
 
             # Lognormal distribution
             if(object@dist=="lnorm") {
 
-              parm_form <- unlist(strsplit(gsub(" ", "", unlist(strsplit(object@formula, ":", fixed=T))[2]), ",", fixed=T))
-
-              # !! parnames e vars sono uguali (ne tengo solo uno)
-
-              parnames <- grep("delta", names(object@call$start), value=T, fixed=T, invert=T)
-
-              if (typeof(parm_form)=="character" && length(parm_form)>0) {
-                ## linear model specified for some parameters
-                vars <- sapply(strsplit(parm_form, "~", fixed=T),"[",1)
-                models <-  paste0("~", sapply(strsplit(parm_form, "~", fixed=T),"[",2))
-              } else {parm_form <- c(); vars <- c(); models <- c()}
-
-              for (i in seq(along=parnames)) {
-                if (!(parnames[i] %in% vars)) {
-                  vars <- c(vars, parnames[i])
-                  models <- c(models, "~1")
-                  parm_form <- c(parm_form, paste0(parnames[i], "~1"))
-                }
-              }
-
-              vpos <- list()
-              for (i in seq(along=parm_form)) {
-                vname <- vars[i]      ## name of variable
-                vpos[[vname]] <- which(parnames==vname)
-              }
-
-            # if(fit@dist=="lnorm") {
-
-              if (is.null(newdata)) {
+              if (is.null(prediction.data)) {
 
               mm1 <- model.matrix(formula(models[vpos[["logitp"]]]), data=as.data.frame(object@data))
               mm2 <- model.matrix(formula(models[vpos[["mu"]]]), data=as.data.frame(object@data))
@@ -73,7 +73,7 @@ setMethod("predict", "wtd",
 
                   if (length(labels(terms(as.formula(parm_form[i]))))!=0) {
 
-                    if ((sum(!labels(terms(as.formula(parm_form[i]))) %in% names(newdata)==T)) >=1) {
+                    if ((sum(!labels(terms(as.formula(parm_form[i]))) %in% names(prediction.data)==T)) >=1) {
 
                       stop("Covariates used in the estimation are not in the prediction dataset (new data)")
 
@@ -83,10 +83,11 @@ setMethod("predict", "wtd",
 
                 }
 
+                mm1 <- model.matrix(formula(models[vpos[["logitp"]]]), data=as.data.frame(prediction.data))
+                mm2 <- model.matrix(formula(models[vpos[["mu"]]]), data=as.data.frame(prediction.data))
+                mm3 <- model.matrix(formula(models[vpos[["lnsigma"]]]), data=as.data.frame(prediction.data))
 
-                mm1 <- model.matrix(formula(models[vpos[["logitp"]]]), data=as.data.frame(newdata))
-                mm2 <- model.matrix(formula(models[vpos[["mu"]]]), data=as.data.frame(newdata))
-                mm3 <- model.matrix(formula(models[vpos[["lnsigma"]]]), data=as.data.frame(newdata))
+                mm_names <- model.frame(formula(models[vpos[["mu"]]]), data=as.data.frame(prediction.data))
 
               }
 
@@ -126,16 +127,70 @@ setMethod("predict", "wtd",
               }
 
               # Weibull distribution
+
             } else if(object@dist=="weib") {
+
+              if (is.null(prediction.data)) {
+
+                mm1 <- model.matrix(formula(models[vpos[["logitp"]]]), data=as.data.frame(object@data))
+                mm2 <- model.matrix(formula(models[vpos[["lnalpha"]]]), data=as.data.frame(object@data))
+                mm3 <- model.matrix(formula(models[vpos[["lnbeta"]]]), data=as.data.frame(object@data))
+
+                mm_names <- model.frame(formula(models[vpos[["lnalpha"]]]), data=as.data.frame(object@data))
+
+              } else {
+
+                for (i in seq_along(parnames)) {
+
+                  if (length(labels(terms(as.formula(parm_form[i]))))!=0) {
+
+                    if ((sum(!labels(terms(as.formula(parm_form[i]))) %in% names(prediction.data)==T)) >=1) {
+
+                      stop("Covariates used in the estimation are not in the prediction dataset (new data)")
+
+                    }
+
+                  }
+
+                }
+
+                mm1 <- model.matrix(formula(models[vpos[["logitp"]]]), data=as.data.frame(prediction.data))
+                mm2 <- model.matrix(formula(models[vpos[["lnalpha"]]]), data=as.data.frame(prediction.data))
+                mm3 <- model.matrix(formula(models[vpos[["lnbeta"]]]), data=as.data.frame(prediction.data))
+
+                mm_names <- model.frame(formula(models[vpos[["lnalpha"]]]), data=as.data.frame(prediction.data))
+
+              }
+
+              # design matrix multiplied by estimates
+              est <- list()
+              for (i in seq_along(parnames)) {
+                vname <- vars[i]      ## name of variable
+                est[[vname]] <- object@coef[grepl(vname, names(object@coef))]
+              }
+
+              # compute parameters (i.e. mu) used to predict duration and probabilities
+              mm <- c("mm1", "mm2", "mm3")
+
+              for (i in seq_along(parnames)) {
+
+                x <- get(mm[i]) %*% matrix(unlist(est[i]))
+                assign(parnames[i], x)
+
+              }
+
+              # browser()
 
               if(type=="dur") {
 
-                lnalpha <- object@fullcoef[2]
-                lnbeta <- object@fullcoef[3]
+                # lnalpha <- object@fullcoef[2]
+                # lnbeta <- object@fullcoef[3]
+
 
                   if(!iadmean) {
 
-                    out <- (-log(1-quantile))^(1/exp(lnalpha))*exp(lnbeta)
+                    out <- (-log(1-quantile))^(1/exp(lnalpha))/exp(lnbeta)
+                    out <- cbind(out, mm_names)
 
                   } else  {
 
@@ -155,13 +210,61 @@ setMethod("predict", "wtd",
               # Exponential distribution
             } else if(object@dist=="exp") {
 
+              if (is.null(prediction.data)) {
+
+                mm1 <- model.matrix(formula(models[vpos[["logitp"]]]), data=as.data.frame(object@data))
+                mm2 <- model.matrix(formula(models[vpos[["lnbeta"]]]), data=as.data.frame(object@data))
+
+                mm_names <- model.frame(formula(models[vpos[["logitp"]]]), data=as.data.frame(object@data))
+
+              } else {
+
+                for (i in seq_along(parnames)) {
+
+                  if (length(labels(terms(as.formula(parm_form[i]))))!=0) {
+
+                    if ((sum(!labels(terms(as.formula(parm_form[i]))) %in% names(prediction.data)==T)) >=1) {
+
+                      stop("Covariates used in the estimation are not in the prediction dataset (new data)")
+
+                    }
+
+                  }
+
+                }
+
+                mm1 <- model.matrix(formula(models[vpos[["logitp"]]]), data=as.data.frame(prediction.data))
+                mm2 <- model.matrix(formula(models[vpos[["lnbeta"]]]), data=as.data.frame(prediction.data))
+
+                mm_names <- model.frame(formula(models[vpos[["logitp"]]]), data=as.data.frame(prediction.data))
+
+              }
+
+              # design matrix multiplied by estimates
+              est <- list()
+              for (i in seq_along(parnames)) {
+                vname <- vars[i]      ## name of variable
+                est[[vname]] <- object@coef[grepl(vname, names(object@coef))]
+              }
+
+              # compute parameters (i.e. mu) used to predict duration and probabilities
+              mm <- c("mm1", "mm2")
+
+              for (i in seq_along(parnames)) {
+
+                x <- get(mm[i]) %*% matrix(unlist(est[i]))
+                assign(parnames[i], x)
+
+              }
+
               if(type=="dur") {
 
-                lnbeta <- object@fullcoef[2]
+                # lnbeta <- object@fullcoef[2]
 
                   if(!iadmean) {
 
                     out <- (-log(1-quantile))/exp(lnbeta)
+                    out <- cbind(out, mm_names)
 
                   } else {
 
@@ -171,7 +274,7 @@ setMethod("predict", "wtd",
 
               } else if(type=="prob") {
 
-                lnbeta <- object@fullcoef[2]
+                # lnbeta <- object@fullcoef[2]
 
                 out <- exp(-exp(lnbeta)*distrx)
               }

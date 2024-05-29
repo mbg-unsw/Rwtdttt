9ae9932cf5548e2236b67e071a72b8c3acf7d38f
sabrinagiometto <sabrina.giometto@gmail.com>
2024-05-29 23:07:17 +0200

    more examples added

--- sandpit/try2.R
+++ sandpit/try2.R
@@ -10,14 +10,14 @@ library(tidyverse)
 # Dataset contains one observation per person, first dispensing for 2014
 df <- haven::read_dta(system.file("extdata", "wtddat_dates.dta", package="Rwtdttt"))
 
+df <- cbind(df, sex = sample(c("M","F"), dim(df)[1], replace = T))
+
 # fit waiting time distribution
 fit1 <- wtdttt(data = df,
                rx1time ~ dlnorm(logitp, mu, lnsigma),
                id = "pid",
-               start = as.Date('2014-01-01'), end = as.Date('2014-12-31'),
-               subset = "rx1time"=="2014-01-01"
+               start = as.Date('2014-01-01'), end = as.Date('2014-12-31'), subset = "sex=='F'"
                )
-
 
 summary(fit1)
 
@@ -92,6 +92,8 @@ fit2 <- wtdttt(data = df,
 
 summary(fit2)
 
+predict(fit2, type = "dur")
+
 ### Use covariate in all three parameter equations ----
 
 # make packsize a factor
@@ -143,10 +145,15 @@ df <- df %>%
        arrange(packsize, distlast) %>%
        mutate(packsize = as.factor(packsize))
 
-prob_pred <- predict(fit1, type = "prob", newdata = df, distrx = df$distlast)
+prob_pred <- predict(fit1, type = "prob", prediction.data = df, distrx = df$distlast)
 
 # df <- df %>% arrange(packsize, distlast)
-plot(df$distlast, prob_pred, type = "l", ylim = c(0,1))
+# plot(df$distlast, prob_pred, type = "l", ylim = c(0,1))
+
+tmp <- cbind(df, prob_pred)
+
+ggplot(data = tmp, aes(x=distlast, y=prob_pred, group = packsize)) +
+  geom_line()
 
 ### try if the model without covariates matches stata results ----
 df <- haven::read_dta(system.file("extdata", "wtddat_covar.dta", package="Rwtdttt"))
@@ -160,7 +167,7 @@ fit1 <- wtdttt(data = df,
 df <- haven::read_dta(system.file("extdata", "lastRx_index.dta", package="Rwtdttt"))
 
 df <- df %>% arrange(packsize, distlast)
-prob_pred <- predict(fit1, type = "prob", newdata = df, distrx = df$distlast)
+prob_pred <- predict(fit1, type = "prob", prediction.data = df, distrx = df$distlast)
 plot(df$distlast, prob_pred, type = "l")
 ggplot(data = df, aes(x=distlast, y=prob_pred, group = packsize)) +
   geom_line() +
@@ -182,13 +189,44 @@ summary(fit1)
 
 predict(fit1, type = "dur", quantile = 0.9)
 
+### with weibull distribution ----
+df <- haven::read_dta(system.file("extdata", "wtddat_covar.dta", package="Rwtdttt"))
+
+df <- df %>%
+  mutate(packsize = as.factor(packsize))
+
+fit1 <- wtdttt(data = df,
+               last_rxtime ~ dweib(logitp, lnalpha, lnbeta),
+               start = 0, end = 1, reverse = T, parameters = list(logitp ~ packsize, lnalpha ~ 1, lnbeta ~ packsize)
+)
+
+summary(fit1)
+
+predict(fit1, type = "dur", quantile = 0.9)
+
+### with exponential distribution ----
+df <- haven::read_dta(system.file("extdata", "wtddat_covar.dta", package="Rwtdttt"))
+
+df <- df %>%
+  mutate(packsize = as.factor(packsize))
+
+fit1 <- wtdttt(data = df,
+               last_rxtime ~ dexp(logitp, lnbeta),
+               start = 0, end = 1, reverse = T, parameters = list(logitp ~ packsize, lnbeta ~ packsize)
+)
+
+summary(fit1)
+
+predict(fit1, type = "dur", quantile = 0.9)
+
+
 ### with a new dataset for prediction ----
 df <- haven::read_dta(system.file("extdata", "lastRx_index.dta", package="Rwtdttt"))
 
 df <- df %>%
   mutate(packsize = as.factor(packsize))
 
-predict(fit1, type = "dur", quantile = 0.9, newdata = df)
+predict(fit1, type = "dur", quantile = 0.9, prediction.data = df)
 
 ### with more than one covariate (after having implemented model.frame in pred_dur_prob to extract var names) ----
 df <- haven::read_dta(system.file("extdata", "wtddat_covar.dta", package="Rwtdttt"))
@@ -210,7 +248,7 @@ df <- df %>%
        mutate(packsize = as.factor(packsize),
               sex = as.factor(sample(c("F","M"), dim(df)[1], replace = T)))
 
-predict(fit1, type = "dur", quantile = 0.9, newdata = df)
+predict(fit1, type = "dur", quantile = 0.9, prediction.data = df)
 
 ### with different covariates in the estimation and prediction dataset (checking the error message) ----
 df <- haven::read_dta(system.file("extdata", "wtddat_covar.dta", package="Rwtdttt"))
@@ -227,7 +265,7 @@ df <- haven::read_dta(system.file("extdata", "lastRx_index.dta", package="Rwtdtt
 
 df <- df %>% mutate(packsize = as.factor(packsize))
 
-predict(fit1, type = "dur", quantile = 0.9, newdata = df)
+predict(fit1, type = "dur", quantile = 0.9, prediction.data = df)
 
 ### with a larger prediction dataset (multiple rows per subject) ----
 df <- haven::read_dta(system.file("extdata", "wtddat_covar.dta", package="Rwtdttt"))
@@ -243,7 +281,7 @@ df <- haven::read_dta(system.file("extdata", "ranwtddat_discdates.dta", package=
 
 df <- df %>% mutate(packsize = as.factor(sample(c(100,200), dim(df)[1], replace = T)))
 
-predict(fit1, type = "dur", quantile = 0.9, newdata = df)
+predict(fit1, type = "dur", quantile = 0.9, prediction.data = df)
 
 
 ## try if the model without covariates matches stata results

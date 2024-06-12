31850bb510ee38248c47aa8ee06891fa3a2ef3bd
sabrinagiometto <sabrina.giometto@gmail.com>
2024-06-12 15:15:31 +0200

    slot added

--- R/wtd-class.R
+++ R/wtd-class.R
@@ -11,10 +11,12 @@
 #' @slot dist name of the WTD distribution family: "lnorm", "weib" or "exp"
 #' @slot depvar name of the dependent variable
 #' @slot idvar (optional) name of the id variable
+#' @slot isreverse logical; indicates whether a forward or backward distribution is used
 #'
 #' @exportClass wtd
 #' @importClassesFrom bbmle mle2
 #'
 setClass("wtd", contains="mle2", slots=c(delta="numeric", dist="character",
-                                         depvar="character", idvar="character"))
+                                         depvar="character", idvar="character",
+                                         isreverse="logical"))
 

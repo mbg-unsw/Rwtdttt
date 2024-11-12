73ad441800bfacb5277768fb773f46ae8551ec46
Malcolm Gillies <Malcolm.Gillies@unsw.edu.au>
2024-11-12 21:32:55 +1100

    Added skeleton of pred_dur_prob tests

---
+++ tests/testthat/test-pred_dur_prob.R
@@ -0,0 +1,15 @@
+# Test predict.wtd
+# * all distributions
+# * date or continuous data
+# * forward or reverse
+# * different values of delta
+# * prediction.data=NULL
+# * type=c("dur", "prob")
+# * iadmean=c(FALSE, TRUE)
+# * distrx=NULL
+# * quantile=0.8
+# * se.fit=c(FALSE, TRUE)
+
+# test_that("multiplication works", {
+#   expect_equal(2 * 2, 4)
+# })

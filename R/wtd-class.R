71fc219906813331b81631b36ce390abaf0e9acd
Malcolm Gillies <Malcolm.Gillies@unsw.edu.au>
2025-02-16 13:09:24 +1100

    Closes #27

--- R/wtd-class.R
+++ R/wtd-class.R
@@ -17,10 +17,12 @@
 #'
 #' @exportClass wtd
 #' @importClassesFrom bbmle mle2 summary.mle2
-#' @importMethodsFrom bbmle summary
 #'
 setClass("wtd", contains="mle2", slots=c(delta="numeric", dist="character",
                                          depvar="character", idvar="character",
                                          isreverse="logical", start="character",
                                          end="character"))
 
+#' @importMethodsFrom bbmle summary
+#' @exportMethod summary
+NULL
